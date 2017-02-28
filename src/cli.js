22657694aa2afee7bf9e11b79a7f724075a37de5
Mikkel Trolle Larsen <trolster2@gmail.com>
2017-02-28 22:04:02 +0100

    Add cli commands in cli.js

---
+++ src/cli.js
@@ -0,0 +1,70 @@
+#!/usr/bin/env node
+
+// npm modules
+import cli from 'commander';
+// our modules
+import pkg from '../package.json';
+import {
+  tokenCmd,
+  certsCmd,
+  setupCmd,
+  assignCmd,
+  moneyCmd,
+  testCmd,
+} from './commands';
+
+cli
+  .version(pkg.version)
+  .usage('<command> <args> [options]');
+
+cli
+  .command('token')
+  .arguments('<newToken>')
+  .description('Save a new token')
+  .action((newToken) => {
+    tokenCmd(newToken);
+  });
+
+cli
+  .command('certs')
+  .description('Save your certifications')
+  .action(() => {
+    certsCmd();
+  });
+
+cli
+  .command('setup')
+  .arguments('<token>')
+  .description('Setup review configuration options.')
+  .action((token) => {
+    setupCmd(token);
+  });
+
+cli
+  .command('assign')
+  .arguments('<ids...>')
+  .option('--push <accessToken>', 'Get push notifications using <accessToken>.')
+  .description('Place requests in the queue.')
+  .action((ids, options) => {
+    assignCmd(ids, options);
+  });
+
+cli
+  .command('money')
+  .arguments('[periods...]')
+  .option('--from <date>', 'select from <date>.')
+  .option('--to <date>', 'select to <date>.')
+  .description('Lets you know how much you\'ve earned in a given period.')
+  .action((periods, options) => {
+    moneyCmd(periods, options);
+  });
+
+cli
+  .command('test')
+  .arguments('<cmd> [accessToken]')
+  .description('Test notifications')
+  .action((cmd, accessToken) => {
+    testCmd(cmd, accessToken);
+  });
+
+export default cli;
---
+++ src/index.js
@@ -0,0 +1,14 @@
+import cli from './cli';
+
+cli.parse(process.argv);
+
+if (!cli.args.length) cli.help();
+
+// output all uncaught exceptions
+process.on('uncaughtException', (err) => {
+  console.error('uncaught exception:', err);
+});
+// output all uncaught promise rejections
+process.on('unhandledRejection', (err) => {
+  console.error('unhandled rejection:', err);
+});
