5ccedf8a53db7670c62c76244215fed64dd27c58
Mikkel Trolle Larsen <trolster2@gmail.com>
2017-02-28 22:08:54 +0100

    Add commands folder

---
+++ src/commands/index.js
@@ -0,0 +1,6 @@
+export {tokenCmd} from './token';
+export {certsCmd} from './certs';
+export {assignCmd} from './assign';
+export {setupCmd} from './setup';
+export {moneyCmd} from './money';
+export {testCmd} from './test';
