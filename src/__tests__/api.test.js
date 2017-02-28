cc3d246997d13d5a61e6a930f48825a6b4067490
Mikkel Trolle Larsen <trolster2@gmail.com>
2017-02-28 22:24:10 +0100

    Rename apiCall.test.js to api.test.js in __tests__

--- src/__tests__/api.test.js
+++ src/__tests__/api.test.js
@@ -1,13 +1,12 @@
 /* global test, expect */
 
-import apiCall from '../apiCall';
-import config from '../config';
+import {api, config} from '../utils';
 
-const token = config.token;
 
-test('apiCall to "count" returns a number', async () => {
+test('api call to "count" returns a number', async () => {
   const task = 'count';
-  const count = await apiCall({token, task});
+  const token = config.token;
+  const count = await api({token, task});
   const assignedCountObject = {
     body: {
       assigned_count: expect.any(Number),
