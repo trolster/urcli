4223c85c9b8dcff4506984fd7db9bd6ed7aa5c12
Mikkel Trolle Larsen <trolster2@gmail.com>
2017-03-22 05:20:13 +0100

    Refactor setup with the inquirer package

--- src/commands/setup.js
+++ src/commands/setup.js
@@ -2,59 +2,42 @@
 import readline from 'readline';
 // npm modules
 import moment from 'moment';
+import inquirer from 'inquirer';
 import chalk from 'chalk';
 // our modules
 import {Api, Config} from '../utils';
 
 const config = new Config();
+let api;
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-  prompt: 'Certified language > ',
-});
-
-export const setupCmd = (token) => {
-  config.token = token;
-  config.tokenAge = moment().add(28, 'd');
-  const api = new Api(token);
-
-  console.log('You can now add all the languages that you are certified for.');
-  console.log('Simply type in each language code and separate them with a space.');
-  console.log('Example: "en-us pt-br"');
-  rl.prompt();
-
-  rl.on('line', (line) => {
-    /* eslint-disable no-underscore-dangle */
-    line.trim();
-    line.toLowerCase();
-    if (line === 'y') {
-      rl.close();
-      return;
-    }
-    const lang = line.match(/[a-z]{2}-[a-z]{2}/gi);
-    if (lang === null) {
-      rl._prompt = line === 'n' ? 'Try again (ex: "en-us pt-br") > ' :
-                                  'Invalid language code(s). Try again (ex: "en-us pt-br") > ';
-      rl.prompt();
-    } else {
-      config.languages = lang;
-      console.log('You have entered the following language(s): ', lang);
-      rl._prompt = 'Was this correct? (y/n) > ';
-      rl.prompt();
-    }
+function getUserInfo() {
+  /* SET FIRST REVIEW DATE */
+  const firstReviewDate = api.call({
+    task: 'completed',
+    body: {
+      start_date: moment('2017-03-21').format('YYYY-MM-DDTHH:mm:ss.SSS'),
+      end_date: moment().format('YYYY-MM-DDTHH:mm:ss.SSS'),
+    },
+  }).then((res) => {
+     // returns the smallest number
+    const firstDate = res.body
+      .map(review => moment(review.assigned_at)) // returns date of review
+      .map(date => date.valueOf()) // returns date in Unix Time (milliseconds from 1970)
+      .reduce((acc, val) => {
+        if (acc < val) {
+          return acc;
+        }
+        return val;
+      });
+    config.startDate = moment(firstDate).format('YYYY-MM-DD');
+    return Promise.resolve();
   });
 
-  rl.on('close', async () => {
-    const task = 'certifications';
-    const certifications = await api({token, task});
-    if (certifications.statusCode !== 200) {
-      console.error(chalk.red('The script encountered an error:'));
-      console.error(chalk.red(`statusCode: ${certifications.statusCode}`));
-      console.error(chalk.red(`This is the token you passed in as an argument: ${token}`));
-      throw new Error();
-    }
-    config.certs = certifications.body
+  /* SET THE CERTIFICATIONS */
+  const certifications = api.call({
+    task: 'certifications'
+  }).then((res) => {
+    const certs = res.body
       .filter(cert => cert.status === 'certified')
       .reduce((acc, cert) => {
         /* eslint-disable no-param-reassign */
@@ -64,6 +47,77 @@ export const setupCmd = (token) => {
         };
         return acc;
       }, {});
-    config.save();
+    config.certs = certs;
+    return Promise.resolve();
   });
-};
+
+  Promise.all([firstReviewDate, certifications]).then(() => {
+    config.save();
+    console.log(config);
+    process.exit(0);
+  })
+}
+
+const accessToken = () => {
+  inquirer.prompt([{
+    type: 'input',
+    name: 'accessToken',
+    message: 'Input your access token:'
+  }]).then((answer) => {
+    Object.assign(config, answer);
+    getUserInfo();
+  })
+}
+
+const pushbullet = () => {
+  inquirer.prompt([{
+    type: 'confirm',
+    name: 'pushbullet',
+    message: 'Do you wish to use PushBullet?',
+    default: false
+  }]).then((answer) => {
+    if (answer.pushbullet) {
+      accessToken();
+    } else {
+      getUserInfo();
+    }
+  })
+}
+
+const languages = () => {
+  inquirer.prompt([{
+    type: 'checkbox',
+    message: 'Select Language(s) that you are certified for:\n',
+    name: 'languages',
+    choices: ['en-us','pt-br','zh-cn','es-es'],
+    validate: function (answer) {
+      if (answer.length < 1) {
+        return 'You must choose at least one language.';
+      }
+      return true;
+    }
+  }]).then((answers) => {
+    Object.assign(config, answers);
+    pushbullet();
+  })
+}
+
+export const setupCmd = () => {
+  inquirer.prompt([{
+    type: 'input',
+    name: 'token',
+    message: 'Input your token:',
+    validate(answer) {
+      api = new Api(answer);
+      return api.call({task: 'count'}).then((res) => {
+        if (res.statusCode == '200') {
+          return true
+        }
+        return 'The token was invalid, try again.';
+      })
+    }
+  }]).then((answer) => {
+    config.token = answer;
+    languages();
+  })
+}
