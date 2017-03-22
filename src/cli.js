80155f5f6d00fe35c57cae838140b194120f05e3
Mikkel Trolle Larsen <trolster2@gmail.com>
2017-03-22 19:35:05 +0100

    Comment out broken commands

--- src/cli.js
+++ src/cli.js
@@ -3,11 +3,11 @@ import cli from 'commander';
 // our modules
 import pkg from '../package.json';
 import {
+  setupCmd,
   tokenCmd,
   certsCmd,
-  setupCmd,
-  assignCmd,
-  revenueCmd,
+  // assignCmd,
+  // revenueCmd,
 } from './commands';
 
 cli
@@ -31,41 +31,30 @@ cli
 
 cli
   .command('setup')
-  .arguments('<token>')
   .description('Setup review configuration options.')
-  .action((token) => {
-    setupCmd(token);
-  });
-
-cli
-  .command('assign')
-  .arguments('<ids...>')
-  .option('--push <accessToken>', 'Get push notifications using <accessToken>.')
-  .option('--feedbacks', 'Get notified on new feedbacks.')
-  .option('--silent', 'Skip the helptext.')
-  .description('Place requests in the queue.')
-  .action((ids, options) => {
-    assignCmd(ids, options);
-  });
-
-cli
-  .command('revenue')
-  .arguments('[periods...]')
-  .option('--from <date>', 'select from <date>.')
-  .option('--to <date>', 'select to <date>.')
-  .description('Lets you know how much you\'ve earned in a given period.')
-  .action((periods, options) => {
-    revenueCmd(periods, options);
-  });
-
-cli
-  .command('money')
-  .arguments('[args]')
-  .description('This command is deprecated. It will be removed in future releases. Use the "revenue" command instead.')
   .action(() => {
-    console.log('The money command has deprecated and will be removed in future releases.');
-    console.log('    Use the "revenue" command instead.');
-    process.exit(0);
+    setupCmd();
   });
+
+// cli
+//   .command('assign')
+//   .arguments('<ids...>')
+//   .option('--push <accessToken>', 'Get push notifications using <accessToken>.')
+//   .option('--feedbacks', 'Get notified on new feedbacks.')
+//   .option('--silent', 'Skip the helptext.')
+//   .description('Place requests in the queue.')
+//   .action((ids, options) => {
+//     assignCmd(ids, options);
+//   });
+
+// cli
+//   .command('revenue')
+//   .arguments('[periods...]')
+//   .option('--from <date>', 'select from <date>.')
+//   .option('--to <date>', 'select to <date>.')
+//   .description('Lets you know how much you\'ve earned in a given period.')
+//   .action((periods, options) => {
+//     revenueCmd(periods, options);
+//   });
 
 export default cli;
